026262f76de5c04f04a1796f36d6654631dec538
JERONIMORAMIREZRESTREPO <jeronimoramirez1159288@correo.itm.edu.co>
2026-03-19 22:46:29 -0500

    Windos application with all of its buttons working.

--- ExpressionEvaluator.UI.Win/Form1.Designer.cs
+++ ExpressionEvaluator.UI.Win/Form1.Designer.cs
@@ -28,12 +28,33 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            textBox1.Location = new Point(12, 12);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(393, 23);
+            textBox1.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            BackColor = SystemColors.Control;
+            ClientSize = new Size(417, 597);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "Functions Evaluator";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private TextBox textBox1;
     }
 }
---
+++ ExpressionEvaluator.UI.WindowsApp/Form1.Designer.cs
@@ -0,0 +1,386 @@
+﻿namespace ExpressionEvaluator.UI.WindowsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            txtDisplay = new TextBox();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btn6 = new Button();
+            btn5 = new Button();
+            btn4 = new Button();
+            btn3 = new Button();
+            btn2 = new Button();
+            btn1 = new Button();
+            btnDot = new Button();
+            btn0 = new Button();
+            btnPlus = new Button();
+            btnMultiply = new Button();
+            btnOpenParenthesis = new Button();
+            btnMinus = new Button();
+            btnDivide = new Button();
+            btnCloseParenthesis = new Button();
+            btnDelete = new Button();
+            btnClear = new Button();
+            btnPow = new Button();
+            btnResult = new Button();
+            SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            txtDisplay.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
+            txtDisplay.BackColor = Color.Green;
+            txtDisplay.Font = new Font("Gill Sans MT", 21.75F);
+            txtDisplay.Location = new Point(12, 12);
+            txtDisplay.Name = "txtDisplay";
+            txtDisplay.Size = new Size(473, 41);
+            txtDisplay.TabIndex = 0;
+            // 
+            // btn7
+            // 
+            btn7.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn7.Font = new Font("Gill Sans MT", 21.75F);
+            btn7.Location = new Point(12, 66);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(65, 59);
+            btn7.TabIndex = 1;
+            btn7.Text = "7";
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btn7_Click;
+            // 
+            // btn8
+            // 
+            btn8.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn8.Font = new Font("Gill Sans MT", 21.75F);
+            btn8.Location = new Point(80, 66);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(65, 59);
+            btn8.TabIndex = 2;
+            btn8.Text = "8";
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btn8_Click;
+            // 
+            // btn9
+            // 
+            btn9.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn9.Font = new Font("Gill Sans MT", 21.75F);
+            btn9.Location = new Point(148, 66);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(65, 59);
+            btn9.TabIndex = 3;
+            btn9.Text = "9";
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btn9_Click;
+            // 
+            // btn6
+            // 
+            btn6.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn6.Font = new Font("Gill Sans MT", 21.75F);
+            btn6.Location = new Point(148, 131);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(65, 59);
+            btn6.TabIndex = 6;
+            btn6.Text = "6";
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btn6_Click_1;
+            // 
+            // btn5
+            // 
+            btn5.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn5.Font = new Font("Gill Sans MT", 21.75F);
+            btn5.Location = new Point(80, 131);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(65, 59);
+            btn5.TabIndex = 5;
+            btn5.Text = "5";
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btn5_Click_1;
+            // 
+            // btn4
+            // 
+            btn4.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn4.Font = new Font("Gill Sans MT", 21.75F);
+            btn4.Location = new Point(12, 131);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(65, 59);
+            btn4.TabIndex = 4;
+            btn4.Text = "4";
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btn4_Click;
+            // 
+            // btn3
+            // 
+            btn3.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn3.Font = new Font("Gill Sans MT", 21.75F);
+            btn3.Location = new Point(148, 196);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(65, 59);
+            btn3.TabIndex = 9;
+            btn3.Text = "3";
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btn3_Click;
+            // 
+            // btn2
+            // 
+            btn2.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn2.Font = new Font("Gill Sans MT", 21.75F);
+            btn2.Location = new Point(80, 196);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(65, 59);
+            btn2.TabIndex = 8;
+            btn2.Text = "2";
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btn2_Click;
+            // 
+            // btn1
+            // 
+            btn1.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn1.Font = new Font("Gill Sans MT", 21.75F);
+            btn1.Location = new Point(12, 196);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(65, 59);
+            btn1.TabIndex = 7;
+            btn1.Text = "1";
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btn1_Click;
+            // 
+            // btnDot
+            // 
+            btnDot.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnDot.Font = new Font("Gill Sans MT", 21.75F);
+            btnDot.Location = new Point(148, 261);
+            btnDot.Name = "btnDot";
+            btnDot.Size = new Size(65, 59);
+            btnDot.TabIndex = 12;
+            btnDot.Text = ".";
+            btnDot.UseVisualStyleBackColor = true;
+            btnDot.Click += btnDot_Click;
+            // 
+            // btn0
+            // 
+            btn0.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btn0.Font = new Font("Gill Sans MT", 21.75F);
+            btn0.Location = new Point(12, 261);
+            btn0.Name = "btn0";
+            btn0.Size = new Size(133, 59);
+            btn0.TabIndex = 10;
+            btn0.Text = "0";
+            btn0.UseVisualStyleBackColor = true;
+            btn0.Click += btn0_Click;
+            // 
+            // btnPlus
+            // 
+            btnPlus.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnPlus.BackColor = Color.FromArgb(255, 128, 0);
+            btnPlus.Font = new Font("Gill Sans MT", 21.75F);
+            btnPlus.Location = new Point(216, 196);
+            btnPlus.Name = "btnPlus";
+            btnPlus.Size = new Size(65, 59);
+            btnPlus.TabIndex = 15;
+            btnPlus.Text = "+";
+            btnPlus.UseVisualStyleBackColor = false;
+            btnPlus.Click += btnPlus_Click;
+            // 
+            // btnMultiply
+            // 
+            btnMultiply.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnMultiply.BackColor = Color.FromArgb(255, 128, 0);
+            btnMultiply.Font = new Font("Gill Sans MT", 21.75F);
+            btnMultiply.Location = new Point(216, 131);
+            btnMultiply.Name = "btnMultiply";
+            btnMultiply.Size = new Size(65, 59);
+            btnMultiply.TabIndex = 14;
+            btnMultiply.Text = "*";
+            btnMultiply.UseVisualStyleBackColor = false;
+            btnMultiply.Click += btnMultiply_Click;
+            // 
+            // btnOpenParenthesis
+            // 
+            btnOpenParenthesis.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnOpenParenthesis.BackColor = Color.FromArgb(255, 128, 0);
+            btnOpenParenthesis.Font = new Font("Gill Sans MT", 21.75F);
+            btnOpenParenthesis.Location = new Point(216, 66);
+            btnOpenParenthesis.Name = "btnOpenParenthesis";
+            btnOpenParenthesis.Size = new Size(65, 59);
+            btnOpenParenthesis.TabIndex = 13;
+            btnOpenParenthesis.Text = "(";
+            btnOpenParenthesis.UseVisualStyleBackColor = false;
+            btnOpenParenthesis.Click += btnOpenParenthesis_Click;
+            // 
+            // btnMinus
+            // 
+            btnMinus.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnMinus.BackColor = Color.FromArgb(255, 128, 0);
+            btnMinus.Font = new Font("Gill Sans MT", 21.75F);
+            btnMinus.Location = new Point(284, 196);
+            btnMinus.Name = "btnMinus";
+            btnMinus.Size = new Size(65, 59);
+            btnMinus.TabIndex = 18;
+            btnMinus.Text = "-";
+            btnMinus.UseVisualStyleBackColor = false;
+            btnMinus.Click += btnMinus_Click;
+            // 
+            // btnDivide
+            // 
+            btnDivide.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnDivide.BackColor = Color.FromArgb(255, 128, 0);
+            btnDivide.Font = new Font("Gill Sans MT", 21.75F);
+            btnDivide.Location = new Point(284, 131);
+            btnDivide.Name = "btnDivide";
+            btnDivide.Size = new Size(65, 59);
+            btnDivide.TabIndex = 17;
+            btnDivide.Text = "/";
+            btnDivide.UseVisualStyleBackColor = false;
+            btnDivide.Click += btnDivide_Click;
+            // 
+            // btnCloseParenthesis
+            // 
+            btnCloseParenthesis.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            btnCloseParenthesis.BackColor = Color.FromArgb(255, 128, 0);
+            btnCloseParenthesis.Font = new Font("Gill Sans MT", 21.75F);
+            btnCloseParenthesis.Location = new Point(284, 66);
+            btnCloseParenthesis.Name = "btnCloseParenthesis";
+            btnCloseParenthesis.Size = new Size(65, 59);
+            btnCloseParenthesis.TabIndex = 16;
+            btnCloseParenthesis.Text = ")";
+            btnCloseParenthesis.UseVisualStyleBackColor = false;
+            btnCloseParenthesis.Click += btnCloseParenthesis_Click;
+            // 
+            // btnDelete
+            // 
+            btnDelete.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnDelete.BackColor = Color.FromArgb(255, 128, 0);
+            btnDelete.Font = new Font("Gill Sans MT", 21.75F);
+            btnDelete.Location = new Point(352, 66);
+            btnDelete.Name = "btnDelete";
+            btnDelete.Size = new Size(133, 59);
+            btnDelete.TabIndex = 19;
+            btnDelete.Text = "Delete";
+            btnDelete.UseVisualStyleBackColor = false;
+            btnDelete.Click += btnDelete_Click;
+            // 
+            // btnClear
+            // 
+            btnClear.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnClear.BackColor = Color.FromArgb(255, 128, 0);
+            btnClear.Font = new Font("Gill Sans MT", 21.75F);
+            btnClear.Location = new Point(352, 131);
+            btnClear.Name = "btnClear";
+            btnClear.Size = new Size(133, 59);
+            btnClear.TabIndex = 20;
+            btnClear.Text = "Clear";
+            btnClear.UseVisualStyleBackColor = false;
+            btnClear.Click += btnClear_Click;
+            // 
+            // btnPow
+            // 
+            btnPow.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnPow.BackColor = Color.FromArgb(255, 128, 0);
+            btnPow.Font = new Font("Gill Sans MT", 21.75F);
+            btnPow.Location = new Point(352, 196);
+            btnPow.Name = "btnPow";
+            btnPow.Size = new Size(133, 59);
+            btnPow.TabIndex = 21;
+            btnPow.Text = "^";
+            btnPow.UseVisualStyleBackColor = false;
+            btnPow.Click += btnPow_Click;
+            // 
+            // btnResult
+            // 
+            btnResult.Anchor = AnchorStyles.Left | AnchorStyles.Right;
+            btnResult.BackColor = Color.FromArgb(255, 128, 0);
+            btnResult.Font = new Font("Gill Sans MT", 21.75F);
+            btnResult.Location = new Point(216, 261);
+            btnResult.Name = "btnResult";
+            btnResult.Size = new Size(269, 59);
+            btnResult.TabIndex = 22;
+            btnResult.Text = "=";
+            btnResult.UseVisualStyleBackColor = false;
+            btnResult.Click += btnResult_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ActiveCaptionText;
+            ClientSize = new Size(497, 338);
+            Controls.Add(btnResult);
+            Controls.Add(btnPow);
+            Controls.Add(btnClear);
+            Controls.Add(btnDelete);
+            Controls.Add(btnMinus);
+            Controls.Add(btnDivide);
+            Controls.Add(btnCloseParenthesis);
+            Controls.Add(btnPlus);
+            Controls.Add(btnMultiply);
+            Controls.Add(btnOpenParenthesis);
+            Controls.Add(btnDot);
+            Controls.Add(btn0);
+            Controls.Add(btn3);
+            Controls.Add(btn2);
+            Controls.Add(btn1);
+            Controls.Add(btn6);
+            Controls.Add(btn5);
+            Controls.Add(btn4);
+            Controls.Add(btn9);
+            Controls.Add(btn8);
+            Controls.Add(btn7);
+            Controls.Add(txtDisplay);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox txtDisplay;
+        private Button btn7;
+        private Button btn8;
+        private Button btn9;
+        private Button btn6;
+        private Button btn5;
+        private Button btn4;
+        private Button btn3;
+        private Button btn2;
+        private Button btn1;
+        private Button btnDot;
+        private Button btn0;
+        private Button btnPlus;
+        private Button btnMultiply;
+        private Button btnOpenParenthesis;
+        private Button btnMinus;
+        private Button btnDivide;
+        private Button btnCloseParenthesis;
+        private Button btnDelete;
+        private Button btnClear;
+        private Button btnPow;
+        private Button btnResult;
+    }
+}
